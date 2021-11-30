2fc4ed5c2a6539e9af15d7158903fc06eec7a80f
elinlow <elin.andersson.lowenadler@gmail.com>
2021-11-30 12:53:19 +0100

    created markup and script sheet

---
+++ script.js
@@ -0,0 +1,18 @@
+// FIRST ASSIGNMENT (week 47, due Friday week 48)
+// Calculate/Compare Area
+
+// Case 1
+
+const triangleOne = (10 * 13) / 2;
+const triangleTwo = (16.5 * 20.3) / 2;
+
+
+// Case 2
+
+const triangleThree = (16.5 * 20.3) / 2;
+const triangleFour = (20.3 * 16.5) / 2;
+
+// Case 3
+
+const triangleFive = (7.8 * 5.6) / 2;
+const triangleSix = (9.3 * 12.4) / 2;
